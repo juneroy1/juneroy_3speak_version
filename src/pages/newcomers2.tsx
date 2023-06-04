d0e70848edb9927ed99d60e67e32d9a5d34ff4c6
Juneroy Quinimon <eroyjune@gmail.com>
2023-06-04 23:44:20 +0800

    feat: added padding for new comers page

--- src/pages/newcomers2.tsx
+++ src/pages/newcomers2.tsx
@@ -71,7 +71,7 @@ const NewComers2 = () => {
         {/* {videos.length} */}
         {videos.map((video, index) => (
           <GridItem w="100%" h="100%" key={index}>
-            <Image backgroundColor={'#222 !important'} alt="test" src={`${video.thumbnail}`} />
+            <Image padding={'5px'} backgroundColor={'#222 !important'} alt="test" src={`${video.thumbnail}`} />
             <Text fontWeight={"bold"} marginY={"10px"} as="h3">
               {video.title}
             </Text>
